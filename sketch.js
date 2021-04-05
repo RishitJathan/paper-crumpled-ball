83dfffa52a69dcea3d3f86927777017266b8733b
RishitJathan <76199211+RishitJathan@users.noreply.github.com>
2021-04-05 19:59:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, dustbin, ball;
+
+function preload() {
+
+backgroundImage=loadImage("GamingBackground.png");	
+
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = Bodies.rectangle(width / 2, 600, width, 5, { isStatic: true });
+	World.add(world, ground);
+
+	dustbin = new Dustbin(600, 590);
+
+	ball = new Paper(100, 550, 20);
+
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+	background(backgroundImage);
+
+	fill("red");
+	text("Dustbin", 580, 570);
+	
+	
+	rectMode(CENTER);
+	fill(255, 192, 203);
+	rect(ground.position.x, ground.position.y, width, 5);
+
+    ball.show();
+
+	dustbin.display();
+
+	
+	drawSprites();
+
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Body.applyForce(ball.body, ball.body.position, { x: 60, y: -55 });
+	}
+}
